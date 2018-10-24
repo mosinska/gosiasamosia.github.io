fdeb6fb0a3703be7c60f96967884f55975e497bf
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-10-24 02:22:12 +0200

    zadanie 04 JS - 02 sortuj string

---
+++ zadania-domowe/04-js/02-sortuj-litery-stringa/js/main.js
@@ -0,0 +1,26 @@
+//Wersja1
+let napis = "Oddzielnie";
+
+function sortowanie(napis){
+    
+    let posortowanyNapis= napis.split('');
+    return posortowanyNapis;
+
+}
+
+console.log(sortowanie(napis));
+
+
+
+
+
+////WERSJA2
+//function odbicie(Odbicie){
+//    
+//    let odwroconyNapis= Odbicie.split('').reverse('').join('');
+//    return odwroconyNapis;
+//
+//}
+//
+//odbicie("Odbicie");
+//console.log(odbicie("Odbicie"));
