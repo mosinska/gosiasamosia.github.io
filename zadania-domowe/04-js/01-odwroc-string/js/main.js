a182003782eedc3f1d9e67b896084ab3e3a7453e
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-10-24 02:22:28 +0200

    zadanie 04 JS - 01 odwróć string

---
+++ zadania-domowe/04-js/01-odwroc-string/js/main.js
@@ -0,0 +1,27 @@
+//Wersja1
+let napis = "Odbicie";
+
+function odbicie(napis){
+    
+    let odwroconyNapis= napis.split('').reverse('').join('');
+    return odwroconyNapis;
+
+}
+
+odbicie("Odbicie");
+console.log(odbicie("Odbicie"));
+
+
+
+
+
+////WERSJA2
+//function odbicie(Odbicie){
+//    
+//    let odwroconyNapis= Odbicie.split('').reverse('').join('');
+//    return odwroconyNapis;
+//
+//}
+//
+//odbicie("Odbicie");
+//console.log(odbicie("Odbicie"));
