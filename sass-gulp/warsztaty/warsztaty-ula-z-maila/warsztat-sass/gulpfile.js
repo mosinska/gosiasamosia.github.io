fa6189d01d4644273d7f8bf6e0299e0449e171d9
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-10-24 02:19:09 +0200

    SASS i GULP z Ulą

---
+++ sass-gulp/warsztaty/warsztaty-ula-z-maila/warsztat-sass/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    'use strict';
+  return gulp.src('app/scss/**/*.scss') 
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'));
+});
+
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+});
