cee4923212e96c8ac82bb941baaa512e0b37ee9a
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-10-24 02:21:39 +0200

    zadanie 04 JS - 04 suma kwadratow

---
+++ zadania-domowe/04-js/04-suma-kwadratow/js/main.js
@@ -0,0 +1,65 @@
+//wilk85
+//function oblicz(arr){
+//    let sum = 0;
+//    for(let i=0; i<arr.length; i++){
+//        sum += Math.pow(arr[i], 2);
+//        
+//    }
+//    return sum;
+//}
+//
+//let wynik = oblicz([1,3,6,4,5]);
+//console.log(`Suma wszystkich elementów podniesionych do kwadratu to: ${wynik}`);
+
+//stachoo99
+//function liczbyDoKwadratu(tablica) {
+//    tablica = [0,1,2,3,4,5];
+//    var suma = 0;
+//    for (var i=0; i<tablica.length; i++) {
+//        suma += Math.pow(tablica[i],2);
+//    }
+//    return suma;
+//    
+//}
+//console.log(liczbyDoKwadratu());
+
+let tab = new Array(0,1,2,3,4,5);
+let sumaKwadratow = 0;
+
+
+for (i = 0; i < 6; i++) {
+//console.log(tab[i]);
+let kwadrat = Math.pow([i],2);
+sumaKwadratow += tab([i]+[i++]);
+
+}
+
+//function wynik(tab){
+console.log("Wynik sumy kwadratów elementów z tablicy: " + dodawanie);
+
+//}
+
+
+// function oblicz(arr){
+//     let sum = 0;
+//     for(let i=0; i<arr.length; i++){
+//         sum += Math.pow(arr[i], 2);
+        
+//     }
+//     return sum;
+// }
+
+// let wynik = oblicz([1,3,6,4,5]);
+// console.log(`Suma wszystkich elementów podniesionych do kwadratu to: ${wynik}`);
+
+
+//let tab = new Array (1,3,6,4,5);
+//
+//function oblicz(tab){
+//    let sumaKwadratow = 0;
+//    for(let i=0; i<tab.length; i++){
+//        sumaKwadratow += Math.pow(tab[i], 2);
+//}
+//
+//let wynik = oblicz(1,3,6,4,5);
+//console.log(`Suma wszystkich elementów podniesionych do kwadratu to: ${wynik}`);
